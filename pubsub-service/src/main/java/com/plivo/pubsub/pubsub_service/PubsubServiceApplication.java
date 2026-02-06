e7033c6e06ed5a70ab76657f625b5d3ddb72c264
Aabhash Sharma <aabhash.aakash@gmail.com>
2026-02-06 12:06:10 +0530

    Add files via upload

---
+++ pubsub-service/src/main/java/com/plivo/pubsub/pubsub_service/PubsubServiceApplication.java
@@ -0,0 +1,13 @@
+package com.plivo.pubsub.pubsub_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PubsubServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PubsubServiceApplication.class, args);
+	}
+
+}
